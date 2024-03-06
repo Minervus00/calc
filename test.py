583ffa3a6c57129f2b25fb9063fa26b37c0ff4e5
Minervus00 <fayouedraogo@protonmail.com>
2024-03-06 15:34:36 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from app import add
+
+
+class TestAddition(unittest.TestCase):
+    def test_deux_zero(self):
+        self.assertEqual(add(1, 2), 3, "1 + 2 = 3")
+
+    def test_decimaux(self):
+        self.assertEqual(add(6.1, 4.7), 10.8)
+
+    def test_negatifs(self):
+        self.assertAlmostEqual(add(5, 0.1), 5.1)
+
+    def test_entiers(self):
+        self.assertEqual(add(5, 7), 12)
